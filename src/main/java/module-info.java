8daeda1c7953b3b76412a3e638b3d906a709cd3e
dassana002 <dassana002@gmail.com>
2026-02-13 23:21:51 +0530

    project init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.pharmacypos_layered {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.pharmacypos_layered to javafx.fxml;
+    exports org.example.pharmacypos_layered;
+}
