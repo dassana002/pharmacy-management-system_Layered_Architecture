c1877c347bb735f5bc4a467fd8a4477583f936d3
dassana002 <dassana002@gmail.com>
2026-02-19 18:57:50 +0530

    implement UI load Luncher

--- src/main/java/org/example/pharmacypos_layered/Launcher.java
+++ src/main/java/org/example/pharmacypos_layered/Launcher.java
@@ -1,9 +1,36 @@
 package org.example.pharmacypos_layered;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
-public class Launcher {
+public class Launcher extends Application{
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // create a scene Object
+        scene = new Scene(loadFXML(""));
+        //scene object set to stage
+        stage.setScene(scene);
+        // scene object pop up
+        stage.show();
+    }
+
+    /// current load  .fxml file load to in this method
+    public static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    public static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("/lk/ijse/pharmacymanagementsystem/" +fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
     public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
+        launch(args);
     }
 }
