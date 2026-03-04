1e0a5ded9657cc9dd22d20b39aaa6eae4229cf91
dassana002 <dassana002@gmail.com>
2026-03-04 08:52:43 +0530

    implement DbConnection class with using Singleton Design Pattern

--- src/main/java/org/example/pharmacypos_layered/db/DbConnection.java
+++ src/main/java/org/example/pharmacypos_layered/db/DbConnection.java
@@ -9,16 +9,16 @@ public class DbConnection {
     private final String USER = "root";
     private final String PASSWORD = "DAka@24381";
 
-    private final Connection connection;
     private static DbConnection dbConnection;
+    private final Connection connection;
 
     private DbConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        connection = DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
     public static DbConnection getInstance() throws SQLException, ClassNotFoundException {
-        if (dbConnection == null) return new DbConnection();
+        if (dbConnection==null) return new DbConnection();
         return dbConnection;
     }
 
