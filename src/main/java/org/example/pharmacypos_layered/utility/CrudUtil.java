59da0675712735aa556042e699824a412770c7cc
dassana002 <dassana002@gmail.com>
2026-03-04 09:04:42 +0530

    implement Crud Utility class and Optimized Query Operations.

--- src/main/java/org/example/pharmacypos_layered/utility/CrudUtil.java
+++ src/main/java/org/example/pharmacypos_layered/utility/CrudUtil.java
@@ -4,28 +4,28 @@ import org.example.pharmacypos_layered.db.DbConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CrudUtil {
-    public static <T>T execute(String query, Object ...object) throws SQLException, ClassNotFoundException {
+
+    public static <T>T execute(String query, Object...object) throws SQLException, ClassNotFoundException {
+
         // get Connection
-        Connection conn = DbConnection.getInstance().getConnection();
+        Connection connection = DbConnection.getInstance().getConnection();
 
-        // set to Connection and Query
-        // create Prepared Statement
-        PreparedStatement ps = conn.prepareStatement(query);
+        // Set Connection to PreparedStatement
+        PreparedStatement ps = connection.prepareStatement(query);
 
-        // set Values to Parameters
-        for (int i = 1; i < object.length; i++) {
+        // Set Values to Parameters
+        for (int i = 1; i <= object.length; i++) {
             ps.setObject(i, object[i]);
         }
 
-        // query start word test
+        // Check Query
         if (query.startsWith("SELECT") || query.startsWith("select")) {
             return (T) ps.executeQuery();
-        }else{
-            return (T)(Boolean) (ps.executeUpdate() > 0);
+        }else {
+            return (T) (Boolean) (0 < ps.executeUpdate());
         }
     }
 }
