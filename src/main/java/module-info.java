b65b358400f6a8ca6e1b3c0e9d85852bf00cb6ff
dassana002 <dassana002@gmail.com>
2026-03-06 11:07:28 +0530

    Update Lombok Dependency version.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,22 +1,14 @@
 module org.example.pharmacypos_layered {
+
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires com.almasb.fxgl.all;
     requires java.sql;
-    requires static lombok;
+    requires mysql.connector.j;
     requires jbcrypt;
-
-    opens org.example.pharmacypos_layered to javafx.fxml;
-    exports org.example.pharmacypos_layered;
+    requires static lombok;
 
     opens org.example.pharmacypos_layered.controller to javafx.fxml;
 
+    exports org.example.pharmacypos_layered;
+    opens org.example.pharmacypos_layered.controller.DashBoard to javafx.fxml;
 }
