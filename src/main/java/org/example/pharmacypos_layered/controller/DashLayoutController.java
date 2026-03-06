c4abe640054ad3b8e01ad566ec0a6352a0158f0f
dassana002 <dassana002@gmail.com>
2026-03-06 12:58:08 +0530

    implement DashLayoutController Class.

---
+++ src/main/java/org/example/pharmacypos_layered/controller/DashLayoutController.java
@@ -0,0 +1,66 @@
+package org.example.pharmacypos_layered.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import org.example.pharmacypos_layered.Launcher;
+import org.example.pharmacypos_layered.utility.References;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashLayoutController implements Initializable {
+
+    @FXML
+    private AnchorPane mainContent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle){
+        try {
+            References.dashLayoutController = this;
+            backToDashBoard();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void backToDashBoard() throws IOException {
+        try {
+            Parent dashBoard = Launcher.loadFXML("pages/DashBoardPage");
+            mainContent.getChildren().setAll(dashBoard);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Load Order Page
+    public void loadOrderPage() {
+        Parent dashBoard = null;
+        try {
+            dashBoard = Launcher.loadFXML("pages/OrderPage");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        mainContent.getChildren().setAll(dashBoard);
+    }
+
+    // Load Employee Page
+    public void loadEmployeePage() throws IOException {
+        Parent medicinePage = Launcher.loadFXML("components/employee/SignUpPage");
+        mainContent.getChildren().setAll(medicinePage);
+    }
+
+    // Load Item Page
+    public void loadItemPage() throws IOException{
+        Parent medicinePage = Launcher.loadFXML("pages/ProductPage");
+        mainContent.getChildren().setAll(medicinePage);
+    }
+
+    @FXML
+    void handleLogOut(ActionEvent event) throws IOException {
+        Launcher.setRoot("LogInPage");
+    }
+}
--- src/main/java/org/example/pharmacypos_layered/utility/References.java
+++ src/main/java/org/example/pharmacypos_layered/utility/References.java
@@ -1,5 +1,10 @@
 package org.example.pharmacypos_layered.utility;
 
+import org.example.pharmacypos_layered.controller.DashBoardController;
+import org.example.pharmacypos_layered.controller.DashLayoutController;
+
 public class References {
     public static String currentUserName;
+    public static DashLayoutController dashLayoutController;
+    public static DashBoardController dashBoardController;
 }
